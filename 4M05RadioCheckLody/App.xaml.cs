46735b37866295af990bc947faf4b41c464a5692
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-10-01 13:42:02 +0200

    Dodaj pliki projektów.

---
+++ 4M05RadioCheckLody/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M05RadioCheckLody
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M05RadioCheckLody/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+namespace _4M05RadioCheckLody
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOblicz(object sender, EventArgs e) 
+        {
+            double cena = 0;
+            int ileGalek = 0;
+            if(int.TryParse(entCount.Text, out ileGalek)) 
+                cena = ileGalek * 7.50;
+            else
+            {
+                lblCena.Text = "Podaj liczbę gałek";
+                return;
+            }
+            if(ileGalek <= 0)
+            {
+                lblCena.Text = "Podaj liczbę gałek";
+                return;
+            }
+            if (rbtDunski.IsChecked)
+                cena += 3;
+            if (chkPolewa.IsChecked)
+                cena += 1;
+            if (chkPosypka.IsChecked)
+                cena += 1;
+            lblCena.Text = $"Cena: {cena.ToString("#.00")} zł";
+        }
+    }
+
+}
